49fa5b130abddce90ac7865702d0ed80dcf52860
Luis Sanchez <sanchez.alberto.luis01@gmail.com>
2022-04-11 10:19:43 -0500

    add try, catch

--- src/ContactsApp.java
+++ src/ContactsApp.java
@@ -3,19 +3,22 @@ package src;
 import src.ContactsManager;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ContactsApp {
-        public static void main(String[] args) {
+
+
+
+
+        public static void main(String[] args) throws IOException {
                 Scanner scanner = new Scanner(System.in);
 
-                //        public static ContactsManager contacts = new ContactsManager();
-                //        public static File contactsFile = new Files();
-                //      public static boolean exitApp
-                //      private static input
 
                 String directory = "data";
                 String filename = "contacts.txt";
@@ -25,30 +28,46 @@ public class ContactsApp {
 
                 System.out.println(dataFile);
 
-                System.out.println("1. View contacts.");
-                System.out.println("2. Add a new contact.");
-                System.out.println("3. Search a contact by name.");
-                System.out.println("4. Delete an existing contact.");
-                System.out.println("5. Exit.");
-                System.out.println("Enter an option (1, 2, 3, 4 or 5):");
-
-                int userInput = scanner.nextInt();
-
-                if (userInput == 1) {
-                        System.out.println("User selected 1");
-                }
-                if (userInput == 2) {
-                        System.out.println("User selected 2");
-                }
-                if (userInput == 3) {
-                        System.out.println("User selected 3");
-                }
-                if (userInput == 4) {
-                        System.out.println("User selected 4");
-                }
-                if (userInput == 5) {
-                        System.out.println("User selected 5");
+                try {
+                        Files.write(
+                                Paths.get("data", "contacts.txt"),
+                                Arrays.asList("eggs"), // list with one item
+                                StandardOpenOption.APPEND
+                        );
+                } catch (IOException e) {
+                        System.err.println("Name not found");
                 }
 
         }
+          public static void menu() {
+        Input userSelection = new Input();
+                        System.out.println("1. View contacts.");
+                        System.out.println("2. Add a new contact.");
+                        System.out.println("3. Search a contact by name.");
+                        System.out.println("4. Delete an existing contact.");
+                        System.out.println("5. Exit.");
+                        System.out.println("Enter an option (1, 2, 3, 4 or 5):");
+
+
+              int userInput = userSelection.getInt();
+
+                        if (userInput == 1) {
+                                System.out.println("User selected 1");
+                        }
+                        if (userInput == 2) {
+                                System.out.println("User selected 2");
+                        }
+                        if (userInput == 3) {
+
+                                System.out.println("User selected 3");
+                        }
+                        if (userInput == 4) {
+                                System.out.println("User selected 4");
+                        }
+                        if (userInput == 5) {
+                                System.out.println("User selected 5");
+                        }
+                }
+
+
 }
---
+++ src/Input.java
@@ -0,0 +1,71 @@
+package src;
+
+import java.util.Scanner;
+
+public class Input {
+    private Scanner input; //lets us take in the user input
+
+
+    public Input() {
+        input = new Scanner(System.in);
+    }
+
+    public String getString() {
+        return input.nextLine();
+    }
+
+    public boolean yesNo() {
+        String userInput = getString();
+        return (userInput.equalsIgnoreCase("y") ||
+                userInput.equalsIgnoreCase("yes"));
+    }
+
+    public int getInt() {
+        String inputInt = this.getString();
+        try {
+
+            return Integer.parseInt(inputInt);
+        } catch (Exception e) {
+            System.out.println("Must enter a valid integer");
+            return getInt();
+        }
+
+    }
+
+    public int getInt(int min, int max) {
+        System.out.println("Please input a number between " + min + " and "
+                + max + ": ");
+
+        int userInt = getInt();
+
+        if(userInt > max || userInt < min) {
+            return getInt(min, max);
+        }
+
+        return userInt;
+    }
+
+    public double getDouble() {
+
+        try {
+            String userInput = this.getString();
+
+            return Double.parseDouble(userInput);
+        } catch (Exception e) {
+            System.out.println("Must enter a valid double:");
+            return getDouble();
+        }
+    }
+
+    public double getDouble(double min, double max) {
+        System.out.println("Please input a number between " + min + " and " + max);
+
+        double userInput = getDouble();
+
+        if (userInput > max || userInput < min ) {
+            return getDouble(min, max);
+        }
+
+        return userInput;
+    }
+}
