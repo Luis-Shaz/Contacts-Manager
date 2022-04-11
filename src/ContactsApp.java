96a29bc258b702c434235abeaac1e17be8caabc3
Shaz Soto-Munet <shaz.soto.munet@gmail.com>
2022-04-11 11:58:13 -0500

    Created addNewContact, searchContacts, and deleteContact methods

--- src/ContactsApp.java
+++ src/ContactsApp.java
@@ -9,65 +9,157 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+
+import static java.nio.file.Files.readAllLines;
 
 public class ContactsApp {
 
-
-
-
         public static void main(String[] args) throws IOException {
-                Scanner scanner = new Scanner(System.in);
 
-
-                String directory = "data";
-                String filename = "contacts.txt";
-
-                Path path = Paths.get(directory);
-                Path dataFile = Paths.get(directory, filename);
-
-                System.out.println(dataFile);
-
-                try {
-                        Files.write(
-                                Paths.get("data", "contacts.txt"),
-                                Arrays.asList("eggs"), // list with one item
-                                StandardOpenOption.APPEND
-                        );
-                } catch (IOException e) {
-                        System.err.println("Name not found");
-                }
+                menu();
 
         }
-          public static void menu() {
-        Input userSelection = new Input();
-                        System.out.println("1. View contacts.");
-                        System.out.println("2. Add a new contact.");
-                        System.out.println("3. Search a contact by name.");
-                        System.out.println("4. Delete an existing contact.");
-                        System.out.println("5. Exit.");
-                        System.out.println("Enter an option (1, 2, 3, 4 or 5):");
 
+        public static void showContacts(){
 
-              int userInput = userSelection.getInt();
+            String directory = "data";
+            String filename = "contacts.txt";
+            Path dataFile = Paths.get(directory, filename);
 
-                        if (userInput == 1) {
-                                System.out.println("User selected 1");
-                        }
-                        if (userInput == 2) {
-                                System.out.println("User selected 2");
-                        }
-                        if (userInput == 3) {
+            try {
 
-                                System.out.println("User selected 3");
-                        }
-                        if (userInput == 4) {
-                                System.out.println("User selected 4");
-                        }
-                        if (userInput == 5) {
-                                System.out.println("User selected 5");
-                        }
+                List<String> contactsFile = Files.readAllLines(dataFile);
+
+                for (int i = 0; i < contactsFile.size(); i += 1) {
+                    System.out.println((i + 1) + ": " + contactsFile.get(i));
                 }
+
+            } catch (IOException error) {
+
+                System.err.println("Name not found");
+
+            }
+
+        }
+
+        public static void addNewContact(String name, String phoneNum) {
+
+            String directory = "data";
+            String filename = "contacts.txt";
+            Path dataFile = Paths.get(directory, filename);
+
+            try {
+                Files.write(
+                        dataFile,
+                        Arrays.asList(name + " | " +  phoneNum), // list with one item
+                        StandardOpenOption.APPEND
+                );
+            } catch (IOException e) {
+                System.err.println("Name not found");
+            }
+
+        }
+
+        public static void searchContacts(String name){
+
+            String directory = "data";
+            String filename = "contacts.txt";
+            Path dataFile = Paths.get(directory, filename);
+
+            try {
+
+                List<String> contactsFile = Files.readAllLines(dataFile);
+
+                for (int i = 0; i < contactsFile.size(); i += 1) {
+
+                    if (contactsFile.get(i).contains(name)){
+                       System.out.println("Contact found!");
+                       System.out.println(contactsFile.get(i));
+                   }
+
+                    if (i == contactsFile.size()-1 && !contactsFile.get(i).contains(name)) {
+                        System.out.println("Name not found.");
+                    }
+
+                }
+
+            } catch (IOException error) {
+
+                System.err.println("Name not found");
+
+            }
+        }
+
+        public static void deleteContact(String name){
+
+            String directory = "data";
+            String filename = "contacts.txt";
+            Path dataFile = Paths.get(directory, filename);
+
+            try {
+
+                List<String> contactsFile = Files.readAllLines(dataFile);
+
+                for (int i = 0; i < contactsFile.size(); i += 1) {
+
+                    if (contactsFile.get(i).contains(name)){
+                        System.out.println("Contact found!");
+                        System.out.println(contactsFile.get(i));
+                    }
+
+                    if (i == contactsFile.size()-1 && !contactsFile.get(i).contains(name)) {
+                        System.out.println("Name not found.");
+                    }
+
+                }
+
+            } catch (IOException error) {
+
+                System.err.println("Name not found");
+
+            }
+        }
+
+        public static void menu() {
+            Input userSelection = new Input();
+
+            System.out.println("1. View contacts.");
+            System.out.println("2. Add a new contact.");
+            System.out.println("3. Search a contact by name.");
+            System.out.println("4. Delete an existing contact.");
+            System.out.println("5. Exit.");
+            System.out.println("Enter an option (1, 2, 3, 4 or 5):");
+
+
+            int userInput = userSelection.getInt();
+
+                if (userInput == 1) {
+                    showContacts();
+                }
+                if (userInput == 2) {
+                    System.out.println("Input name");
+                    String name = userSelection.getString();
+                    System.out.println("Input 10 digit phone number");
+                    String phoneNum = userSelection.getString();
+                    addNewContact(name, phoneNum);
+                    System.out.println("Contact successfully added");
+                }
+                if (userInput == 3) {
+                    System.out.println("Please input the name to be searched");
+                    String name = userSelection.getString();
+                    searchContacts(name);
+                }
+                if (userInput == 4) {
+                    System.out.println("Which contact do you want to delete?");
+                    String name = userSelection.getString();
+                    deleteContact(name);
+                }
+                if (userInput == 5) {
+                        System.out.println("User selected 5");
+                }
+            }
 
 
 }
--- src/ContactsManager.java
+++ src/ContactsManager.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class ContactsManager {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
     }
 
 
